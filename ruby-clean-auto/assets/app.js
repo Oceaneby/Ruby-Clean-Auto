20efc6155267a4bffc19856c80319b1fed89bd87
Obellay <oceanebellay85340@gmail.com>
2025-07-07 00:05:27 +0200

    navbar responsive + carousel vidéo + curseur SVG animé + sections texte

--- ruby-clean-auto/assets/app.js
+++ ruby-clean-auto/assets/app.js
@@ -8,3 +8,44 @@
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const cursor = document.getElementById('cursor');
+//   const trail = document.getElementById('cursor-trail');
+
+//   let mouseX = 0, mouseY = 0;
+//   let trailX = 0, trailY = 0;
+//   let lastX = 0, lastY = 0;
+//   let angle = 0;
+
+//   window.addEventListener('mousemove', e => {
+//     mouseX = e.clientX;
+//     mouseY = e.clientY;
+
+//     trail.style.opacity = '1';
+//   });
+
+//   function animate() {
+//     // Interpolation douce
+//     trailX += (mouseX - trailX) * 0.15;
+//     trailY += (mouseY - trailY) * 0.15;
+
+//     // Calcul angle (en degrés) selon direction mouvement
+//     const dx = mouseX - lastX;
+//     const dy = mouseY - lastY;
+//     angle = Math.atan2(dy, dx) * (180 / Math.PI);
+
+//     lastX = mouseX;
+//     lastY = mouseY;
+
+//     trail.style.transform = `translate(${trailX}px, ${trailY}px) translate(-50%, -50%) rotate(${angle}deg)`;
+//     cursor.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
+
+//     requestAnimationFrame(animate);
+//   }
+//   animate();
+
+//   window.addEventListener('mouseout', () => {
+//     trail.style.opacity = '0';
+//   });
+// });
